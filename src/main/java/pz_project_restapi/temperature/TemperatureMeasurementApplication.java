25c9d795f1cbc2b18a72a071b1479681dcc453ff
ZPavlina <pavlinazimmermann@gmail.com>
2022-08-26 09:55:49 +0200

    Initial commit

---
+++ src/main/java/pz_project_restapi/temperature/TemperatureMeasurementApplication.java
@@ -0,0 +1,13 @@
+package pz_project_restapi.temperature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureMeasurementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureMeasurementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pz_project_restapi/temperature/TemperatureMeasurementApplicationTests.java
@@ -0,0 +1,13 @@
+package pz_project_restapi.temperature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureMeasurementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
